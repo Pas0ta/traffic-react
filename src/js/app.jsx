6bc4fecd7e924e306ce7def8aa9bd2f04e3c2e3e
Pas0ta <ssantamariarodrigo@gmail.com>
2023-01-19 17:51:11 +0000

    ya funciona

---
+++ src/js/app.jsx
@@ -0,0 +1,34 @@
+import React, { Component} from "react";
+import './app.css';
+
+//definicion estados
+class TrafficLight extends Component {
+  constructor(){
+    super();
+      this.state = {
+        estadoRoja: "apagada",
+        estadoAmarilla: "apagada",
+        estadoVerde: "apagada"
+      };
+  }
+    // Semaforo
+    render(){
+      return(
+        <div>
+          <div id="barra"></div>
+          <div id="semaforo">
+            <div className= "luz roja" id= {this.state.estadoRoja} 
+            onClick={()=>this.setState({estadoRoja: "encendida", estadoAmarilla: "apagada", estadoVerde: "apagada"})}>
+            </div>
+            <div className= "luz amarilla" id= {this.state.estadoAmarilla} 
+            onClick={()=>this.setState({estadoRoja: "apagada", estadoAmarilla: "encendida", estadoVerde: "apagada"})}>
+            </div>
+            <div className= "luz verde" id= {this.state.estadoVerde} 
+            onClick={()=>this.setState({estadoRoja: "apagada", estadoAmarilla: "apagada", estadoVerde: "encendida"})}>
+            </div>
+          </div>
+        </div>);
+    }
+}
+
+export default TrafficLight;
